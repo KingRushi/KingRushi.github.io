120da4c15dd0cf8e5accc4e0faf390126c2b5365
Rushi Shah <rushi@Rushis-MacBook-Pro.local>
2020-05-22 15:21:20 -0700

    added Google analytics

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c58dcd8931e71f1d09962f3db4fc06ff.js"
+  "/precache-manifest.0a1b173c97239521c0cbd9815ea38e4d.js"
 );
 
 self.addEventListener('message', (event) => {
