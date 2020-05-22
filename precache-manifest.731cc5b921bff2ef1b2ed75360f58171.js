6249b4dbfc4217b257f1ef5e96baa372a185435a
Rushi Shah <rushi@Rushis-MacBook-Pro.local>
2020-05-21 17:22:29 -0700

    you did it

---
+++ precache-manifest.731cc5b921bff2ef1b2ed75360f58171.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63084cbe12195901aed358d97b94b980",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07984f1a5cf50033ecf7",
+    "url": "/static/css/main.70a01226.chunk.css"
+  },
+  {
+    "revision": "6e642b76b4148657694a",
+    "url": "/static/js/2.93fe020e.chunk.js"
+  },
+  {
+    "revision": "07984f1a5cf50033ecf7",
+    "url": "/static/js/main.81a76987.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
